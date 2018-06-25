5d552edb4c3ffcf23c456f0c35b52621d9d949c6
Alexandre Cardinal <alexandre.cardinal50@gmail.com>
2018-06-25 19:13:46 -0400

    Fixed big number format

--- tests/test-variance.py
+++ tests/test-variance.py
@@ -13,12 +13,12 @@ class TestVariance(unittest.TestCase):
   def testLowerBoundVariance(self):
     dataset = [MIN_VALUE, MIN_VALUE, 0, 0, 10]
     variance = calculator.variance(dataset)
-    self.assertEqual(variance, 1383505809823183688)
+    self.assertEqual(variance, 1.3835058098231836e+18)
 
   def testUpperBoundVariance(self):
     dataset = [MAX_VALUE, 0, 0]
     variance = calculator.variance(dataset)
-    self.assertEqual(variance, 1537228671377473536)
+    self.assertEqual(variance, 1.5372286713774733e+18)
 
   def testInvalidVariance(self):
     dataset = ['ok', 2, 3, 4, 5, 6]
@@ -28,7 +28,7 @@ class TestVariance(unittest.TestCase):
   def testNormalVariance(self):
     dataset = [1, 10, 1, 10, 1 , 10]
     variance = calculator.variance(dataset)
-    self.assertEqual(variance, 24)
+    self.assertEqual(variance, 24.3)
 
 
 
